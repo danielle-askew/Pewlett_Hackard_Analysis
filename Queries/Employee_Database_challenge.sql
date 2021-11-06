45fbcd5b94e4a7ec5605a9157508ec5eb9814e75
danielle-askew <89716711+danielle-askew@users.noreply.github.com>
2021-11-06 17:22:11 -0600

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,46 @@
+-- Deliverable 1 : The Number of Retiring Employees by Title
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+--INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+--INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY emp_no ASC, to_date DESC;
+
+-- Employee count by most recent job title
+SELECT COUNT(ut.emp_no), ut.title
+--INTO retiring_titles
+FROM unique_titles as ut
+Group By title
+Order By Count(title) DESC;
+
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentor_eligibility 
+FROM employees AS e
+INNER JOIN dept_emp AS de 
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+order by e.emp_no;
